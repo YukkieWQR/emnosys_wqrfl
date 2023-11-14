1a52a402d709c8a6f390f0eecb0a9206b15b4dbb
YukkieWQR <pawwne27@gmail.com>
2023-11-14 18:36:14 +0300

    added model

--- emnosys/models.py
+++ emnosys/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import RegexValidator
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
 
+    message = models.TextField()
 
+    nickname = models.CharField(
+        max_length=30,
+        validators=[RegexValidator(r'^@.+$', 'Nickname must start with "@"')],
+    )
