9654ecc01628bd5aad72a716e3f8b0bf0cc8f118
YukkieWQR <pawwne27@gmail.com>
2023-12-02 17:11:15 +0300

    added .gitignore and configured settings.py for sending emails

--- emnosys_wqrfl/settings.py
+++ emnosys_wqrfl/settings.py
@@ -117,13 +117,13 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+from passwords import emailpassword
 
-
-EMAIL_BACKEND = "django.core.mail.backends.smpt.EmailBackend"
-EMAIL_HOST = "emnosy.wqrfl.gmail.com"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT  = 587
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = "emnosy.wqrfl@gmail.com"
+EMAIL_HOST_PASSWORD = emailpassword
 
 
