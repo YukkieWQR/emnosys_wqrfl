3b1c2d72d2ac0baaba829606b5dcc9e0d76c0f2e
YukkieWQR <pawwne27@gmail.com>
2023-12-10 17:56:40 +0300

    moved SendEmail() fuction to views.py and deleted utils.py

--- emnosys/views.py
+++ emnosys/views.py
@@ -16,7 +16,10 @@ from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .utils import SendEmail
+from django.conf import settings
+from django.core.mail import send_mail, EmailMessage
+from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 ####################################################
@@ -102,5 +105,18 @@ def ContactCreateView(request):
 
 
 def SendEmailView(request):
-    SendEmail(request)
+    username = request.user.username
+    all_contacts = Contact.objects.filter(contactowner__username=username)
+
+    for cntct in all_contacts:
+        personalized_message = cntct.message
+        email = EmailMessage(
+            'Someone used our application to send you this emergency message!',
+            personalized_message,
+            'emnosy.wqrfl@gmail.com',
+            [cntct.email],
+        )
+        email.content_subtype = 'html'  # Set the content type to HTML
+        email.send(fail_silently=False)
+
     return redirect('/')
